4fe2cf6a5fee5f74b234231e16dd709848a39220
Artarand <167556760+Artarand@users.noreply.github.com>
2024-05-04 09:36:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 const clickEvent = document.querySelector("img");
 const affichageClick = document.querySelector("h1");
-const Xdeux = document.querySelector("h3");
-const Xtrois = document.querySelector("h4");
-const Xquatre = document.querySelector("h5");
 let clickNumber = 0;
-let numberClickOnX2 = 0;
-let numberClickOnX3 = 0;
-let numberClickOnX4 = 0;
 let clickX = 0;
-let X2auto;
-let X3auto;
-let X4auto;
 let genButtonFactor = 2;
 
 
@@ -28,13 +19,9 @@ let Xautogen = setInterval(() => {
     fragment.firstChild.replaceWith(...elements);
     let factor = 1
 
-    genButton(factor += 1);
-    genButton(factor += 1);
-    genButton(factor += 1);
+    // genButton(factor += 1);
 
-    if (clickNumber > factor * factor * 50) {
-        genButton(factor += 1);
-        genButton(factor += 1);
+    for (price = 0; price < clickNumber; price = factor * factor * 50) {
         genButton(factor += 1);
     }
 }, 1000);
@@ -52,6 +39,14 @@ const genButton = (factor) => {
     button.addEventListener("click", function (e) {
         X(factor);
     });
+    if (clickNumber >= factor * factor * 50) {
+        button.addEventListener("mouseenter", () => {
+            button.style.opacity = 0.8;
+        });
+        button.addEventListener("mouseleave", () => {
+            button.style.opacity = 1;
+        });
+    }
 }
 
 const delButton = (factor) => {
@@ -99,125 +94,3 @@ clickEvent.addEventListener("click", () => {
     clickNumber++;
     affichageClick.textContent = clickNumber;
 });
-
-// const X2 = () => {
-//     if (numberClickOnX2 < 1) {
-//         if (clickNumber > 100) {
-//             clearInterval(X1auto);
-//             X2auto = setInterval(() => {
-//                 clickNumber++;
-//                 affichageClick.textContent = clickNumber;
-//             }, 500);
-//             clickNumber -= 100;
-//             numberClickOnX2++;
-//             Xdeux.classList.add("done");
-//             clearInterval(X3auto);
-//             clearInterval(X4auto);
-//             Xtrois.classList.remove("done");
-//             Xquatre.classList.remove("done");
-//             if (numberClickOnX3 == 1) {
-//                 numberClickOnX3 = 0;
-//                 clickNumber += 500;
-//             } else if (numberClickOnX4 == 1) {
-//                 numberClickOnX2 = 0;
-//                 clickNumber += 2500;
-//             }
-//         } else {
-//             alert("Il vous faut 100 clicks pour acheter cette amélioration.");
-//         }
-//     }
-// };
-
-// const X3 = () => {
-//     if (numberClickOnX3 < 1) {
-//         if (clickNumber > 500) {
-//             clearInterval(X1auto);
-//             X3auto = setInterval(() => {
-//                 clickNumber++;
-//                 affichageClick.textContent = clickNumber;
-//             }, 333);
-//             clickNumber -= 500;
-//             numberClickOnX3++;
-//             Xtrois.classList.add("done");
-//             clearInterval(X2auto);
-//             clearInterval(X4auto);
-//             Xdeux.classList.remove("done");
-//             Xquatre.classList.remove("done");
-//             if (numberClickOnX2 == 1) {
-//                 numberClickOnX2 = 0;
-//                 clickNumber += 100;
-//             } else if (numberClickOnX4 == 1) {
-//                 numberClickOnX2 = 0;
-//                 clickNumber += 2500;
-//             }
-//         } else {
-//             alert("Il vous faut 500 clicks pour acheter cette amélioration.");
-//         }
-//     }
-// };
-
-// const X4 = () => {
-//     if (numberClickOnX4 < 1) {
-//         if (clickNumber > 2500) {
-//             clearInterval(X1auto);
-//             X4auto = setInterval(() => {
-//                 clickNumber++;
-//                 affichageClick.textContent = clickNumber;
-//             }, 250);
-//             clickNumber -= 2500;
-//             numberClickOnX4++;
-//             Xquatre.classList.add("done");
-//             clearInterval(X2auto);
-//             clearInterval(X3auto);
-//             Xdeux.classList.remove("done");
-//             Xtrois.classList.remove("done");
-//             if (numberClickOnX2 == 1) {
-//                 numberClickOnX2 = 0;
-//                 clickNumber += 100;
-//             } else if (numberClickOnX3 == 1) {
-//                 numberClickOnX3 = 0;
-//                 clickNumber += 500;
-//             }
-//         } else {
-//             alert("Il vous faut 2500 clicks pour acheter cette amélioration.");
-//         }
-//     }
-// };
-
-
-// let X1auto = setInterval(() => {
-//     clickNumber++;
-//     affichageClick.textContent = clickNumber;
-// }, 1000);
-
-// Xdeux.addEventListener("click", X2);
-
-// Xtrois.addEventListener("click", X3);
-
-// Xquatre.addEventListener("click", X4);
-
-
-
-// Xdeux.addEventListener("mouseenter", () => {
-//     Xdeux.style.opacity = 0.75;
-// });
-
-// Xdeux.addEventListener("mouseleave", () => {
-//     Xdeux.style.opacity = 1;
-// });
-
-// Xtrois.addEventListener("mouseenter", () => {
-//     Xtrois.style.opacity = 0.75;
-// });
-
-// Xtrois.addEventListener("mouseleave", () => {
-//     Xtrois.style.opacity = 1;
-// });
-
-// Xquatre.addEventListener("mouseenter", () => {
-//     Xquatre.style.opacity = 0.75;
-// });
-
-// Xquatre.addEventListener("mouseleave", () => {
-//     Xquatre.style.opacity = 1;
-// });
